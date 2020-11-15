8f11529528aca601d09d011bb45fa4abdd2de334
Amelia Kwiecinska <kwam0015@gmail.com>
2020-11-15 22:21:59 +0100

    Separate function definitions and declarations

--- matrixclass.cpp
+++ matrixclass.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-class matrix{
+/*class matrix{
 
     int **matrixTab;
     int c, r;
@@ -48,4 +48,73 @@ class matrix{
             cout << endl;
         }
     }
-};
+};*/
+
+matrix::matrix(int n, int m)
+{
+    c = n;
+    r = m;
+    matrixTab = new int *[m];
+    for(int i = 0; i < m; i++)
+    {
+        matrixTab[i] = new int [n]; 
+        for(int j = 0; j < n; j++)
+        {
+            matrixTab[i][j] = 0;
+        }    
+    }
+}
+
+matrix::matrix(int n)
+{
+    c = n;
+    r = n;
+    matrixTab = new int *[n];
+    for(int i = 0; i < n; i++)
+    {
+        matrixTab[i] = new int [n];
+        for(int j = 0; j < n; j++)
+        {
+            matrixTab[i][j] = 0;
+        }
+    }
+}
+
+void matrix::print()
+{
+    for(int i = 0; i < r; i++)
+       {
+           cout << r - i - 1 << "  ";
+        for(int j = 0; j < c; j++)
+        {
+            cout << matrixTab[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "   ";
+    for(int i = 0; i < c; i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+void matrix::set(int n, int m, int val)
+{
+    matrixTab[n][m] = val;
+}
+
+int matrix::get(int n, int m)
+{
+    return matrixTab[n][m];
+}
+
+int matrix::cols()
+{
+    return c;
+}
+
+int matrix::rows()
+{
+    return r;
+}
--- matrixclass.hpp
+++ matrixclass.hpp
@@ -1,6 +1,23 @@
-/*#ifndef matrixclass_hpp
+#ifndef matrixclass_hpp
 #define matrixclass_hpp
 
-class matrix;
+class matrix{
 
-#endif*/
+    private:
+
+    int **matrixTab;
+    int c, r;
+
+    public:
+
+    matrix(int n, int m);
+    matrix(int n);
+    void print();
+    void set(int n, int m, int val);
+    int get(int n, int m);
+    int cols();
+    int rows();
+
+};
+
+#endif
